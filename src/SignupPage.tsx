3fb12a99efdb21bb93fadd65cad32852019ee78e
Alina <alina.kobru@gmail.com>
2026-01-16 23:14:48 -0700

    Created LoginPage

---
+++ src/SignupPage.tsx
@@ -0,0 +1,41 @@
+import "./SignupPage.css";
+
+export default function SignupPage() {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        
+        <h2 className="login-title">Login to your account</h2>
+        
+      </div>
+
+      <div className="login-container">
+        <div className="login-card">
+          <form className="login-form">
+            <div>
+              <label className="login-label">Email address</label>
+              <input type="email" className="login-input" />
+            </div>
+
+            <div>
+              <label className="login-label">Password</label>
+              <input type="password" className="login-input" />
+            </div>
+
+            <a href="#" className="login-forgot">
+              Forgot password?
+            </a>
+
+            <button type="submit" className="login-button">
+              Login
+            </button>
+            <a href="#" className="create-account">
+              Create account?
+        </a>
+          </form>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
